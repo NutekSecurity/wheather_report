7252ce214be9b9e6c117228699e99f28b811231b
Szymon Błaszczyński <museyoucoulduse@gmail.com>
2023-07-13 00:21:15 +0200

    Better + Stocks

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Ichimoku-CCI_H1-D
-Python script  that fetches data from _Oanda API_, saves **Ichimoku Cloud** and **Commodity Channel Index** plots and in other file **MACD** and **RSI** for Daily and 1 hour timeperiod.
+# Wheather Report
+Python script  that fetches data from _Oanda_ or _Finnhub_ API, saves 
+**Ichimoku Cloud** and **Commodity Channel Index** plots and in other file 
+**MACD** and **RSI** for Daily and 1 hour timeperiod - useful for real trading,
+unless you are looking for daytrading help, then that's not what you're looking
+for. Intended for patient and well versed traders tired of looking at charts 
+from their platform. Simplified view of microeconomics to midsized trends that
+occur on Forex and Stock market. Are you ready to take a leap of faith and pursue
+the dangerous path of not starring day and night at your _crypto_ wallet?
 
 ## Examples
 
@@ -15,53 +22,71 @@ Python script  that fetches data from _Oanda API_, saves **Ichimoku Cloud** and 
 
 ## Usage
 
-You will need Python 3 and this libraries (some are included in certain Python distributions - I have used `Anaconda Python`)
+You will need Python 3 (__3.10!__ _MAX_) and libraries included in `requirements.txt` file
 
-```python
-import pandas as pd
-import numpy as np
-import requests
-import json
-from datetime import datetime, timedelta
-import mplfinance as mpf
-import matplotlib.pyplot as plt
-import talib
-import os
-import sys
-import argparse
+```shell
+# for virtualenv
+pip install -r requirements.txt
+# for python3
+python3 -m pip install -r requirements.txt
 ```
+
+Invoke above command to install required modules.
 
 Then you can get simple help using this command:
 
 ```python
-python3 ichi_cci_h1_d.py --help
+python3 wheater_report.py --help
 ```
 
 This should output:
 
 ```bash
-usage: ichi_cci_h1_d.py [-h] --token TOKEN --account ACCOUNT [--practice]
+usage: wheather_report.py [-h] --token TOKEN [--account ACCOUNT] [--practice] [--instruments INSTRUMENTS] [--list]
+                          [--stocks]
 
-optional arguments:
-  -h, --help         show this help message and exit
-  --token TOKEN      Enter your Oanda API token
-  --account ACCOUNT  Enter your account id
-  --practice         Wheter to use Oanda practice account
+Wheather Report v0.1
+This program should help you get the most out of the market.
+Just let the money flow, and don't swim with the sharks!.
+Author: Szymon Błaszczyński for Nutek Security Solutions
+
+::::    ::: :::    ::: ::::::::::: :::::::::: :::    :::
+:+:+:   :+: :+:    :+:     :+:     :+:        :+:   :+:
+:+:+:+  +:+ +:+    +:+     +:+     +:+        +:+  +:+
++#+ +:+ +#+ +#+    +:+     +#+     +#++:++#   +#++:++
++#+  +#+#+# +#+    +#+     +#+     +#+        +#+  +#+
+#+#   #+#+# #+#    #+#     #+#     #+#        #+#   #+#
+###    ####  ########      ###     ########## ###    ###
+
+
+Example usage:
+python3 weather_report.py --token <your_oanda_token> --account <your_account_id> --instruments EUR_USD,USD_JPY,GBP_USD
+python3 weather_report.py --token <your_oanda_token> --account <your_account_id> --instruments all
+python3 weather_report.py --token <your_oanda_oken> --account <your_account_id> --list
+python3 weather_report.py --token <your_oanda_demo_token> --account <your_account_id> --practice --instruments EUR_USD,USD_JPY,GBP_USD
+python3 weather_report.py --token <your_finnhub_token> --stocks --instruments AAPL,MSFT,AMZN
+python3 weather_report.py --token <your_finnhub_token> --stocks --list | less
+
+options:
+  -h, --help            show this help message and exit
+  --token TOKEN         Enter your Oanda API token
+  --account ACCOUNT     Enter your account id (required for Oanda)
+  --practice            Wheter to use Oanda practice account
+  --instruments INSTRUMENTS
+                        Enter the instruments to download plots or all
+  --list                List all instruments
+  --stocks              Wheter to use stocks data (provide Finnhub API key as --token)
 ```
 
-- **token** - your Oanda API token - visit [getting starded on oanda.com](https://developer.oanda.com/rest-live-v20/introduction/) for more
-  information about how to get this token (you can get one for a demo account) - this argument is required
-- **account** - you have to provide an account id from which the data will be fetched - this argument is required
-- **practice** - if you give this command, this program will change the source of information to demo aka practice account - this argument is optional
-
-When you run the program like this:
-
-```bash
-python3 ichi_cci_h1_d.py --token xxx-xxx --account xxx-xxx
-```
-
-it will download the data from Oanda.com and save the charts/plots to the folder relative to it's invocation path and you will have beatiful Ichimoku
-Cloud with Volume, Commodity Channel Index and price in one place for future reference.
+- **token** - your Oanda/Finnhub API token - visit [getting starded on oanda.com](https://developer.oanda.com/rest-live-v20/introduction/) for more
+  information about how to get this token (you can get one for a demo account)
+  or [register on Finnhub](https://finnhub.io/register) - this argument is required
+- **account** - you have to provide an account id from which the data will be fetched - this argument is required for Oanda requests
+- **practice** - if you give this command, this program will change the source of information to demo aka practice account - this argument is optional and
+intended for use with Oanda type of requests, if you prefer to use DEMO account.
+- **stock** - insist you want to have an information about some stock/s
+- **instruments** - choose what you want to get, one or more (separeted by coma or space), even all (_not_ recommended)
+- **list** - get list of US stocks available to show (--stock), or Oanda instruments.
 
 ## License
 
---
+++ wheather_report.py
@@ -0,0 +1,315 @@
+import pandas as pd
+import numpy as np
+import requests
+from datetime import datetime
+import mplfinance as mpf
+import matplotlib.pyplot as plt
+import talib
+import os
+import argparse
+from argparse import RawTextHelpFormatter
+from progress1bar import ProgressBar
+import finnhub
+
+def plotting(frame,title,path):
+    
+    mc = mpf.make_marketcolors(up='g',down='r',alpha=0.5,volume={'up':'g','down':'r'})
+    s  = mpf.make_mpf_style(base_mpf_style='yahoo',marketcolors=mc,gridstyle='')
+    
+    fig = mpf.figure(style=s,figsize=(16,9))
+    ax1 = fig.add_subplot(3,1,1)
+    last_date = frame.index[-1]
+    title_of_plot = "{} Ichiomku and CCI - {}".format(title, str(last_date.strftime('%Y-%m-%d-T%H_%M_%SZ')))
+    ax1.set_title(title_of_plot)
+    ax2 = fig.add_subplot(3,1,2,sharex=ax1)
+    ax3 = fig.add_subplot(3,1,3,sharex=ax1)
+    high9=frame.High.rolling(9).max()
+    Low9=frame.High.rolling(9).min()
+    high26=frame.High.rolling(26).max()
+    Low26=frame.High.rolling(26).min()
+    high52=frame.High.rolling(52).max()
+    Low52=frame.High.rolling(52).min()
+
+    frame['tenkan_sen']=(high9+Low9)/2
+    frame['kijun_sen']=(high26+Low26)/2
+    frame['chikou']=frame.Close.shift(-26)
+    frame=frame.shift(-26)
+    frame['senkou_A']=((frame.tenkan_sen+frame.kijun_sen)/2).shift(26)
+    frame['senkou_B']=((high52+Low52)/2).shift(26)
+
+    cci = talib.CCI(frame['High'], frame['Low'], frame['Close'], timeperiod=20)
+    frame['cci'] = cci
+
+    frame['cci_overbought'] = [100.]*len(frame)
+    frame['cci_oversold'] = [-100.]*len(frame)
+    apds = [mpf.make_addplot(frame['cci'],ax=ax3,color='g',ylabel='CCI',mav=(20)),
+            mpf.make_addplot(frame[ ['cci_overbought','cci_oversold'] ], ax=ax3, linestyle='-.',secondary_y=False),
+            mpf.make_addplot(frame['tenkan_sen'],ax=ax1),
+            mpf.make_addplot(frame['kijun_sen'],ax=ax1),
+            mpf.make_addplot(frame['chikou'],ax=ax1),
+            mpf.make_addplot(frame.senkou_A,ax=ax1,color='magenta',alpha=0.5),
+            mpf.make_addplot(frame.senkou_B,ax=ax1,color='red',alpha=0.5)]
+
+    ax1.fill_between([x for x in range(0,len(frame))],frame.senkou_A,frame.senkou_B,where=frame.senkou_A>=frame.senkou_B,color='lightgreen')
+    ax1.fill_between([x for x in range(0,len(frame))],frame.senkou_A,frame.senkou_B,where=frame.senkou_A<frame.senkou_B,color='lightcoral')
+    ax3.fill_between([x for x in range(0,len(frame))],frame.cci,frame.cci_overbought,where=frame.cci>frame.cci_overbought,color='lightcoral')
+    ax3.fill_between([x for x in range(0,len(frame))],frame.cci,frame.cci_oversold,where=frame.cci<frame.cci_oversold,color='lightgreen')
+    mpf.plot(frame,ax=ax1,volume=ax2,type='line',xrotation=0,addplot=apds,tight_layout=True)
+    fig.savefig("{}/{}.png".format(path, title_of_plot))
+    plt.close(fig)
+    
+    # MACD and RSI chart
+    fig = mpf.figure(style=s,figsize=(16,9))
+    ax1 = fig.add_subplot(4,1,1)
+    ax2 = fig.add_subplot(4,1,2,sharex=ax1)
+    ax3 = fig.add_subplot(4,1,3,sharex=ax1)
+    ax4 = fig.add_subplot(4,1,4,sharex=ax1)
+    last_date = frame.index[-1]
+    title_of_plot = "{} MACD and RSI - {}".format(title, str(last_date.strftime('%Y-%m-%d-T%H_%M_%SZ')))
+    ax1.set_title(title_of_plot)
+    # Calculate the MACD
+    exp12     = frame['Close'].ewm(span=12, adjust=False).mean()
+    exp26     = frame['Close'].ewm(span=26, adjust=False).mean()
+    macd      = exp12 - exp26
+    signal    = macd.ewm(span=9, adjust=False).mean()
+    histogram = macd - signal
+
+    fb_green = dict(y1=macd.values,y2=signal.values,where=signal<macd,color="#93c47d",alpha=0.6,interpolate=True)
+    fb_red   = dict(y1=macd.values,y2=signal.values,where=signal>macd,color="#e06666",alpha=0.6,interpolate=True)
+    fb_green['panel'] = 1
+    fb_red['panel'] = 1
+    fb       = [fb_green,fb_red]
+    # Calculate the RSI
+    rsi = talib.RSI(frame["Close"], timeperiod=14)
+    frame['rsi'] = rsi
+    frame['rsi_overbought'] = [70.]*len(frame)
+    frame['rsi_oversold'] = [30.]*len(frame)
+    apds = [#mpf.make_addplot(exp12,color='lime'),
+            #mpf.make_addplot(exp26,color='c'),
+            mpf.make_addplot(histogram,type='bar',width=0.7,
+                            color='dimgray',alpha=1,secondary_y=True,ax=ax2),
+            mpf.make_addplot(macd,
+                             ylabel='MACD',
+                             color='fuchsia',secondary_y=False,ax=ax2),
+            mpf.make_addplot(signal,
+                             color='b',secondary_y=False,ax=ax2),
+            mpf.make_addplot(rsi, 
+                            ylabel='RSI',
+                               ax=ax3),
+            mpf.make_addplot(frame['rsi_overbought'], 
+                               linestyle='-.',secondary_y=False, color='red',ax=ax3),
+            mpf.make_addplot(frame['rsi_oversold'], 
+                               linestyle='-.',secondary_y=False, color='green',ax=ax3),
+        ]
+    ax3.fill_between([x for x in range(0,len(frame))],frame.rsi,frame.rsi_overbought,where=frame.rsi>frame.rsi_overbought,color='lightcoral')
+    ax3.fill_between([x for x in range(0,len(frame))],frame.rsi,frame.rsi_oversold,where=frame.rsi<frame.rsi_oversold,color='lightgreen')
+    # Plot the data
+    mpf.plot(frame,
+             ax=ax1,
+             volume=ax4,
+             type='line',xrotation=0,addplot=apds,tight_layout=True,
+             fill_between=fb)
+    fig.savefig("{}/{}.png".format(path, title_of_plot))
+    plt.close(fig)
+
+
+def parse_ohlc(response):
+    # Extract candle data from response
+    data = response.json()['candles']
+    df = pd.DataFrame(data)
+    df = df[['time', 'bid', 'volume']]
+
+    # Convert time to datetime and set as index
+    df['time'] = pd.to_datetime(df['time'])
+    df.set_index('time', inplace=True)
+
+    # Convert mid prices to float
+    ohlc = df['bid'].apply(pd.Series).astype(float)
+
+    ohlc['volume'] = df['volume']
+
+    # Rename columns
+    ohlc.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+    return ohlc
+
+def parse_finnohlc(response):
+    # Extract candle data from response
+    df = pd.DataFrame(response)
+    # convert to datetime
+    df['time'] = pd.to_datetime(df['t'], unit='s')
+    # set index
+    df.set_index('time', inplace=True)
+
+    # Convert mid prices to float
+    ohlc = df[['c', 'h', 'l', 'o']].apply(pd.Series).astype(float)
+
+    ohlc['volume'] = df['v']
+
+    # Rename columns
+    ohlc.columns = ['Close', 'High', 'Low', 'Open', 'Volume']
+    return ohlc
+
+program_description = '''Wheather Report v0.1
+This program should help you get the most out of the market.
+Just let the money flow, and don't swim with the sharks!.
+Author: Szymon Błaszczyński for Nutek Security Solutions
+
+\033[1;31m::::    ::: :::    ::: ::::::::::: :::::::::: :::    :::
+\033[1;31m:+:+:   :+: :+:    :+:     :+:     :+:        :+:   :+:
+\033[1;32m:+:+:+  +:+ +:+    +:+     +:+     +:+        +:+  +:+
+\033[1;32m+#+ +:+ +#+ +#+    +:+     +#+     +#++:++#   +#++:++
+\033[1;32m+#+  +#+#+# +#+    +#+     +#+     +#+        +#+  +#+
+\033[1;34m#+#   #+#+# #+#    #+#     #+#     #+#        #+#   #+#
+\033[1;34m###    ####  ########      ###     ########## ###    ###
+\033[0m
+
+Example usage:
+python3 weather_report.py --token <your_oanda_token> --account <your_account_id> --instruments EUR_USD,USD_JPY,GBP_USD
+python3 weather_report.py --token <your_oanda_token> --account <your_account_id> --instruments all
+python3 weather_report.py --token <your_oanda_oken> --account <your_account_id> --list
+python3 weather_report.py --token <your_oanda_demo_token> --account <your_account_id> --practice --instruments EUR_USD,USD_JPY,GBP_USD
+python3 weather_report.py --token <your_finnhub_token> --stocks --instruments AAPL,MSFT,AMZN
+python3 weather_report.py --token <your_finnhub_token> --stocks --list | less
+
+'''
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser(description=program_description, formatter_class=RawTextHelpFormatter)
+# Add a named argument
+parser.add_argument('--token', type=str, help='Enter your Oanda API token', required=True)
+parser.add_argument('--account', type=str, help='Enter your account id (required for Oanda)', required=False)
+parser.add_argument('--practice', action='store_true', help='Wheter to use Oanda practice account')
+parser.add_argument('--instruments', type=str, help='Enter the instruments to download plots or all', required=False)
+parser.add_argument('--list', action='store_true', help='List all instruments')
+parser.add_argument('--stocks', action='store_true', help='Wheter to use stocks data (provide Finnhub API key as --token)')
+# Parse the command-line arguments
+args = parser.parse_args()
+
+if args.stocks:
+    finnhub_client = finnhub.Client(api_key=args.token)
+    if args.list:
+        instruments = finnhub_client.stock_symbols('US')
+        # get only symbols
+        instruments = [x['symbol'] for x in instruments]
+        # sort alphabetically
+        instruments.sort()
+        for instrument in instruments:
+            print(instrument)
+        exit()
+    # time now in milliseconds
+    now = datetime.now()
+    # convert to seconds
+    now = now.timestamp()
+    # 1250 hours ago
+    then_h = now - 1250 * 60 * 60
+    # 1250 days ago
+    then_d = now - 1250 * 24 * 60 * 60
+    # Format datetime as string
+    now_utc = datetime.utcnow()
+    formatted_date = now_utc.strftime('%Y-%m-%d-T%H_%M_%SZ')
+    # specify the directory path
+    directory_path = "finance_charts-{}".format(formatted_date)
+    # check if directory exists, if not then create it
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        print(f"Directory created at {directory_path}")
+    else:
+        print(f"Directory already exists at {directory_path}")
+    instruments = []
+    if args.instruments == 'all':
+        # get all instruments
+        instruments = finnhub_client.stock_symbols('US')
+        # get only symbols
+        instruments = [x['symbol'] for x in instruments]
+    else:
+        instruments = args.instruments.split(',')
+        if len(instruments) < 1:
+            instruments = args.instruments.split(' ')
+            if len(instruments) < 1:
+                instruments = args.instruments
+                if len(instruments) < 1:
+                    print('No instruments provided')
+                    exit(1)
+    print("Downloading {} instruments".format(len(instruments)))
+    completed_message = 'Download and chart drawing complete'
+    with ProgressBar(total=len(instruments), completed_message=completed_message, clear_alias=True, show_fraction=False, show_prefix=False, show_duration=True) as pb:
+        for instrument in instruments:
+            pb.alias = instrument
+            # Stock candles
+            res_d = finnhub_client.stock_candles(instrument, 'D', int(then_d), int(now))
+            res_h1 = finnhub_client.stock_candles(instrument, '60', int(then_h), int(now))
+            # save to csv
+            # df.to_csv('{}.csv'.format(instrument))
+            # plot
+            ohlc_d = parse_finnohlc(res_d)
+            ohlc_h1 = parse_finnohlc(res_h1)
+            plotting(ohlc_d,  "{} - {}".format(instrument, "Day"), directory_path)
+            plotting(ohlc_h1,  "{} - {}".format(instrument, "1 Hour"), directory_path)
+            pb.count += 1
+    exit()
+
+if args.practice:
+    uri = 'https://api-fxpractice.oanda.com/v3/'
+else:
+    uri = 'https://api-fxtrade.oanda.com/v3/'
+# Set up the API endpoint URL and request headers
+endpoint = '{}accounts/{}/instruments/'.format(uri, args.account)
+headers = {'Authorization': 'Bearer {}'.format(args.token),
+           'Content-Type': 'application/json'}
+response = requests.get(endpoint, headers=headers)
+instruments = response.json()['instruments']
+if args.list:
+    # sort alphabetically
+    instruments.sort(key=lambda x: x['name'])
+    for instrument in instruments:
+        print(instrument['name'])
+    exit()
+now = datetime.utcnow()
+# Get current datetime
+# Format datetime as string
+formatted_date = now.strftime('%Y-%m-%d-T%H_%M_%SZ')
+# specify the directory path
+directory_path = "finance_charts-{}".format(formatted_date)
+# check if directory exists, if not then create it
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
+    print(f"Directory created at {directory_path}")
+else:
+    print(f"Directory already exists at {directory_path}")
+inst_len = len(instruments)
+if args.instruments == 'all':
+    pass
+else:
+    instruments_args = args.instruments.split(',')
+    if len(instruments_args) == 0:
+        instruments_args = args.instruments.split(' ')
+        if len(instruments_args) == 0:
+            print("Instrument {} not found".format(args.instruments))
+            exit()
+    instruments = [x for x in instruments if x['name'] in instruments_args]
+    if len(instruments) == 0:
+        print("Instrument {} not found".format(args.instruments))
+        exit()
+print("Downloading {} instruments".format(len(instruments)))
+completed_message = 'Download and chart drawing complete'
+with ProgressBar(total=len(instruments), completed_message=completed_message, clear_alias=True, show_fraction=False, show_prefix=False, show_duration=True) as pb:
+    for index, instrument in enumerate(instruments):
+        pb.alias = instrument['name']
+        instrument_name = instrument['name']
+        # Set up the API endpoint URL and request headers
+        endpoint = '{}instruments/{}/candles'.format(uri, instrument_name)
+        headers = {'Authorization': 'Bearer {}'.format(args.token),
+            'Content-Type': 'application/json'}
+        # Set up the request parameters
+        params = {'price': 'B',
+            'count': 1250,
+            'granularity': 'H1'}
+        response_h1 = requests.get(endpoint, headers=headers, params=params)
+        params = {'price': 'B',
+            'count': 1250,
+            'granularity': 'D'}
+        response_d = requests.get(endpoint, headers=headers, params=params)
+        ohlc_h1 = parse_ohlc(response_h1)
+        ohlc_d = parse_ohlc(response_d)   
+        plotting(ohlc_h1, "{} - {}".format(instrument_name, "1 Hour"), directory_path)
+        plotting(ohlc_d,  "{} - {}".format(instrument_name, "Day"), directory_path)
+        pb.count += 1
